e5b58352a779477856b32dcf1dc9a468364db2d3
Sophia <sophiambataru@gmail.com>
2023-10-01 20:30:52 +0300

    Add package.json file, add app.js server entry file. Spin up express server with a simple testing home route

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
-    res.send('Hello World ...');
+    res.send('Testing Testing ...');
 });
 app.listen(PORT, () => {
     console.log('Server started on port ${PORT}');
