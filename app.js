f58209c5fed848748c0733f90ef4cd18b261e74c
Sophia <sophiambataru@gmail.com>
2023-10-22 18:58:11 +0300

    Add webpack bundling on client and server. Add handlebars templates

--- app.js
+++ app.js
@@ -1,41 +1,55 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const hbs = require('express-handlebars');
-const pool = require('./db');
+const expressSanitizer = require('express-sanitizer');
+const hbsHelpers = require('./views/helpers');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(expressSanitizer());
+
+// setup express routes
+const mainRoutes = require('./routes');
+const booksRoutes = require('./routes/books');
+const musicRoutes = require('./routes/music');
+const moviesRoutes = require('./routes/movies');
+const comicsRoutes = require('./routes/comics');
+
+app.use(mainRoutes);
+app.use('/books', booksRoutes);
+app.use('/music', musicRoutes);
+app.use('/movies', moviesRoutes);
+app.use('/comics', comicsRoutes);
+
+app.use('/static', express.static('public'));
+
 // setup handlebars template engine
-app.engine('hbs', hbs.engine({
-
+app.engine('hbs', hbs({
     extname: 'hbs',
-    defaultlayout: 'main',
+    defaultLayout: 'main',
     layoutsDir: './views/layouts/',
-    partialsDir:'./views/includes/'
-
+    partialsDir: './views/includes/',
+    helpers: {
+        if_equal: hbsHelpers.isEqualHelper
+    }
 }));
 
 app.set('view engine', 'hbs');
 
-app.get('/', (req, res) => {
-    res.render('helloworld');
-});
+// 404 route
 app.use((req, res, next) => {
-    const err = new Error('Page not found');
+    const err = new Error(`The requested URL ${req.originalUrl} was not found on this server.  That's all we know.`);
     err.status = 404;
     next(err);
 });
 
+// error handler
 app.use((err, req, res, next) => {
-
-    res.status(err.status).send(
-        `
-        <h1>${err.status}</h1>
-        <h2>Error${err.messae}</h2> 
-        <p>Stack:${err.stack}</p>
-
-        `
-    );
+    err.status = err.status || 500;
+    res.status(err.status).render('error', { err });
 });
 
 app.listen(PORT, () => {
